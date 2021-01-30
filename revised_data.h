def78380c6632ad5d440fbef72a1b228066c7413
Allen <70372144+allendlr@users.noreply.github.com>
2021-01-31 05:46:48 +0800

    Update revised_data.h

--- revised_data.h
+++ revised_data.h
@@ -6,37 +6,40 @@ using namespace std;
 
 struct Movie {
   int movie_code = 0;
-  string movie_title;
-  string movie_genre;
+  string movie_title = " ";
+  string movie_genre = " ";
   int year_released = 0;
 };
 
 class MovieList {
- private:
-   struct MovieNode {
-     Movie mov[100];
-     Movie data;
-     MovieNode *next = NULL;
+	private:
+	struct MovieNode {
+		Movie data;
+		MovieNode *next;
    };
-    MovieNode *head = NULL;
-    int id = 0;
- public:
-  // Constructor
-   MovieList();
-  // Insert a Movie
-   void InsertNode(int code, string title, string genre, int year);
-  // Rent a Movie
-   void DeleteNode(int code);
-  // Return a Movie
-   void AppendNode(int code, string title, string genre, int year);
-  // Show Movie Details
-   void TraverseList(int code);
-  // Print Movie List
-   void DisplayList();
-  // Destructor
-  ~MovieList();
-};
+	MovieNode *rent_head;
+	MovieNode *head;
+		public:
 
-
-
+		// Constructor
+		MovieList();
+		// Insert a Movie
+		void InsertNode(int code, string title, string genre, int year);
+		// Rent a Movie
+		void DeleteNode(int code);
+		void rentInsert(int code, string title, string genre,int year);
+		void rentDelete(int code);
+		// Return a Movie
+		void AppendNode(int code);
+		bool rentReturn(int code);
+		// Show Movie Details
+		void TraverseList(int code);
+		// Print Movie List
+		void DisplayList();
+    bool isDuplicate(int code);
+		void getCode(int* code);
+		void getMovieData(string* title, string* genre, int* year);
+		// Destructor
+		~MovieList();
+}; 
 #endif
